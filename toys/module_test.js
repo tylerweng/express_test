24b925d380de883f33f857bd0bc70d130ff0eaab
Tyler Weng <tylercweng@gmail.com>
2017-05-18 19:43:56 -0700

    add test exports and imports

---
+++ toys/module_test.js
@@ -0,0 +1,4 @@
+const square = require("./square");
+
+console.log(square.area(4));
+console.log(square.perimeter(2));
---
+++ toys/square.js
@@ -0,0 +1,22 @@
+// Fat Arrow Notation
+
+module.exports = {
+  area: width => (width * width),
+  perimeter: width => (4 * width)
+};
+
+// Function Declaration
+
+// module.exports = {
+//   area: function(width) {
+//     return width * width;
+//   },
+//   perimeter: function(width) {
+//     return 4 * width;
+//   }
+// };
+
+// Individual imports
+
+// exports.area = function(width) { return width * width; };
+// exports.perimeter = function(width) { return 4 * width; };
